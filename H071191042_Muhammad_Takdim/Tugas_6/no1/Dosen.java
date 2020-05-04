cb27ebadb25272969881f5b052004343cc981562
takdim <takdim253@gmail.com>
2020-05-04 19:30:47 +0800

    Tugas_6

---
+++ H071191042_Muhammad_Takdim/Tugas_6/no1/Dosen.java
@@ -0,0 +1,17 @@
+/**
+ * Dosen
+ */
+package no1;
+public class Dosen extends Pegawai{
+    //bonus per sks
+    int plus = 120000;
+
+    @Override //value = sks
+    public void bonus(String name, int value) {
+        int total = (value*plus) + gaji;
+        System.out.println(name + " mendapatkan gaji Rp. " + total);
+    }
+
+    
+    
+}

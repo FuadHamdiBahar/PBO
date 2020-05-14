d9858020961811a441f38554b1ad3d85b19bf260
Ilhamsyah <andiilhamsyahidris@gmail.com>
2020-05-14 21:40:26 +0800

    Tugas 7

---
+++ H071191016_ANDIILHAMSYAHIDRIS/Tugas 7/Main.java
@@ -0,0 +1,23 @@
+class Main {
+
+    public static void main(String[] args) {
+        Integer number = 0;
+        Race race = new Race(number);
+        Runner[] runners = new Runner[10];
+    
+        for (int i = 0; i < runners.length; i++) 
+        {
+          runners[i] = new Runner(++number);
+          runners[i].joinRace(race);
+        }
+    
+        race.start();
+    
+        for (Runner runner : runners) 
+        {
+          runner.start();
+        }
+    
+        race.result();
+    }
+}
